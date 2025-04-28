f8acb55c58df3d1f73ead5fd97d77bbd6f4f088c
Carter Opperman <asq2am@virginia.edu>
2025-04-28 10:54:04 -0400

    remove kill run script

--- kill_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# If necessary, use this to cleanup dangling processes from a benchmark run
-pkill -f timeout && echo "All killed"
-
-
-#kill -- -$(ps -o pgid= $(pgrep -f "run_benchmark.sh") | grep -o [0-9]*)
